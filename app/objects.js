5902e2fac0beb482b643643f0bd02ab5141a23d8
Matt McNeil <mattmcneil.ns@gmail.com>
2018-08-09 15:01:02 +0200

    partially passing tests TODO: complete

--- app/objects.js
+++ app/objects.js
@@ -3,10 +3,16 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+		return fn.call(obj);
     },
 
     alterObjects : function(constructor, greeting) {
+
+	
+		constructor.defineProperty = [greeting,"greeting"];
+		
+		console.log(constructor);
+	
 
     },
 
