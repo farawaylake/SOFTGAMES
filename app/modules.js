51c1ab99ca08226b31775f5e8cc970b4ca7ea4b5
Matt McNeil <mattmcneil.ns@gmail.com>
2018-08-09 13:48:53 +0200

    modules test passing

--- app/modules.js
+++ app/modules.js
@@ -3,8 +3,22 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     createModule : function(str1, str2) {
-
+		var greeting = str1;
+		var name = str2;
+		
+		function sayIt() {
+			greeting = this.greeting;
+			name = this.name;
+			return greeting + ', ' + name;
+		}
+		
+		return {
+			greeting : greeting,
+			name : name,
+			sayIt: sayIt
+		}
+		
     }
-  };
+  }
 });
 
