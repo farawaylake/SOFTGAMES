7d89508dd49641e2ecbb57ac3d2dc111f91fe6eb
Matt McNeil <mattmcneil.ns@gmail.com>
2018-08-09 16:06:07 +0200

    numbers tests passing

--- app/numbers.js
+++ app/numbers.js
@@ -3,19 +3,26 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     valueAtBit: function(num, bit) {
-
+		binaryString = num.toString(2);
+		return parseInt(binaryString[binaryString.length - bit]);
     },
 
     base10: function(str) {
-
+		return parseInt(str,2);
     },
 
     convertToBinary: function(num) {
-
+		binaryString = num.toString(2);
+		
+		if (binaryString % 4 != 0){
+			binaryString = 0 + binaryString;
+		}
+		
+		return binaryString;
     },
 
     multiply: function(a, b) {
-
+		return +(a*b).toFixed(8);
     }
   };
 });
