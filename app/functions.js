100d5ac431c5d9137f07fe844d431f025a83f35d
Matt McNeil <mattmcneil.ns@gmail.com>
2018-08-09 17:58:44 +0200

    completed some tests - may revisit

--- app/functions.js
+++ app/functions.js
@@ -3,31 +3,54 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     argsAsArray : function(fn, arr) {
-
+		return fn.apply(this,arr);
     },
 
     speak : function(fn, obj) {
-
+		return fn.call(obj);
     },
 
     functionFunction : function(str) {
-
+		var localString = str
+		function createOutput(str) {
+			return localString + ', ' + str;
+		}
+		return createOutput;
     },
 
     makeClosures : function(arr, fn) {
-
+		
     },
 
     partial : function(fn, str1, str2) {
 
     },
 
-    useArguments : function() {
+    useArguments : function(a, b, c, d) {
+		var total = 0;
 
+		if (a != null){
+			total = total + a;
+		}
+		if (b != null){
+			total = total + b;
+		}
+		
+		if (c != null){
+			total = total + c;
+		}
+		
+		if (d != null){
+			total = total + d;
+		}
+		
+		return total;
+			
     },
 
-    callIt : function(fn) {
 
+    callIt : function(fn, ...args) {
+		fn.apply(null,args);
     },
 
     partialUsingArguments : function(fn) {
