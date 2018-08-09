e8cd4b0b74639fc50e9877b4cca79aecddf452c0
Matt McNeil <mattmcneil.ns@gmail.com>
2018-08-09 19:13:56 +0200

    count partially completed - TODO

--- app/count.js
+++ app/count.js
@@ -3,6 +3,23 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function () {
   return {
     count : function (start, end) {
+		var counter = start;
+		
+		var timer = setInterval(function(){
+		  console.log(counter);
+		  counter++;
+		  if (counter == end+1) {
+			clearInterval(timer);
+		  }
+		}, 100);	
+		
+		//get this to work
+		//function cancel(){
+		//	clearInterval(timer);
+		//}
+		
+		
+		
     }
   };
 });
