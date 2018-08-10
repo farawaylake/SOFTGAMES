c56b5831d091ba916625f43e06397ce6875fb295
Matt McNeil <mattmcneil.ns@gmail.com>
2018-08-10 12:59:53 +0200

    permutation test passing

--- app/recursion.js
+++ app/recursion.js
@@ -31,10 +31,25 @@ define(function() {
 
     },
 
-    permute: function(arr) {
-		var temp = perm(arr);
-		//console.log(temp);
-		return temp;
+    permute: function(inputArr) {
+		var results = [];
+
+		  function permute(arr, memo) {
+			var cur, memo = memo || [];
+
+			for (var i = 0; i < arr.length; i++) {
+			  cur = arr.splice(i, 1);
+			  if (arr.length === 0) {
+				results.push(memo.concat(cur));
+			  }
+			  permute(arr.slice(), memo.concat(cur));
+			  arr.splice(i, 0, cur[0]);
+			}
+
+			return results;
+		  }
+
+		  return permute(inputArr);
     }
   };
 });
